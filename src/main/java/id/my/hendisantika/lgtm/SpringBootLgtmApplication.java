3d9bb5258bb09d43e72f9ab9f2ed51b944749c99
hendisantika <hendisantika@yahoo.co.id>
2025-07-26 07:57:26 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/lgtm/SpringBootLgtmApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.lgtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLgtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLgtmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/lgtm/SpringBootLgtmApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.lgtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLgtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
